0b63d1007324ad577835fda7458e5a4ac27ecede
Genix <bitsgenix@gmail.com>
2026-05-30 01:12:01 +0330

    reafctor: initial modulariztion c codebase (#18)
    * refactor: remove sdl
    
    * chore: update readme.md
    
    * refactor(lib): remove lib(s) (#14)
    
    * refactor: remove sdl (#15)
    
    * chore: update
    
    * chore(ci): update
    
    * refactor: remove fonts and we can use default fonts system in feature
    
    * chore: update readme.md
    
    * chore: udpate license
    
    * chore: remove icon
    
    * feat(makefile): add build system cross platform makefiles
    
    * feat: add lua detect
    
    * feat(mk): update makefiles and build systems
    
    * refactor(makefiels): update makefiles refactoring to simple
    
    * feat: add NOTICE and update LICENSE
    
    * chore: update
    
    * chore: update
    
    * refactor: remove sdl (#16)
    
    * feat: add sdl3.dll
    
    * feat: add building guides
    
    * feat: add global config
    
    * chore: add empty files
    
    * refactor(plugins): remove data/plugins and add to plugins dir
    
    * refactor: refactor global config
    
    * reafctor: initial modulariztion c codebase

---
+++ src/global_config.c
@@ -0,0 +1,19 @@
+/* This file is just a central place to define global constants.*/
+#include "global_config.h"
+const char *CDIN_APP_NAME    = "cdin";
+const char *CDIN_APP_VERSION =
+#ifdef CDIN_VERSION
+  CDIN_VERSION;
+#else
+  "0.0.0-dev";
+#endif
+
+const char *CDIN_APP_COMMIT =
+#ifdef CDIN_COMMIT
+  CDIN_COMMIT;
+#else
+  "unknown";
+#endif
+
+
+#include "global_config.h"
