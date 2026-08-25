0ba93a1ac201e56f035e2afad0d4f56b8962d572
kasra <kasraam385@gmail.com>
2026-08-25 14:21:51 +0330

    feat(shadcn-tailwind): added ShadcnUI & TailwindCSS

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
-  return <div>Hello world man.</div>;
+  return (
+    <div>
+      Hello world man.
+      <Button>Click me</Button>
+    </div>
+  );
 }
