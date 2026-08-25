629c398554e4ff371526be732730550212af96ac
kasra <kasraam385@gmail.com>
2026-08-25 17:16:26 +0330

    fear(font): add fira code via embed code

--- website/src/pages/Layout.tsx
+++ website/src/pages/Layout.tsx
@@ -1,10 +1,13 @@
 import BottomBar from "@/components/BottomBartion/BottomBar";
-import Navbar from "@/components/Navigations/navbar";
+import Navbar from "@/components/Navigations/Navbar";
 import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div className="max-w-7xl min-h-svh grid grid-cols-1 grid-rows-[auto_1fr_auto] overflow-x-hidden border-x mx-auto">
+    <div
+      id="fira-code-mono"
+      className="max-w-7xl min-h-svh grid grid-cols-1 grid-rows-[auto_1fr_auto] overflow-x-hidden border-x mx-auto"
+    >
       <Navbar />
       <Outlet />
       <BottomBar />
