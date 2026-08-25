05196b9c2d6231477f49cc294dd71c583f6353cd
kasra <kasraam385@gmail.com>
2026-08-25 17:03:49 +0330

    fix(layout): has solid structure

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function App() {
   return (
     <div>
-      Hello world man.
       <Button>Click me</Button>
+      Hello world man.
     </div>
   );
 }
--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import AboutPage from "./pages/AboutUs.tsx";
+import DownloadPage from "./pages/DownloadPage.tsx";
+import DocsPage from "./pages/DocsPage.tsx";
+import NotFound from "./components/NotFound.tsx";
+import Layout from "./pages/Layout.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+          <Route path="about-us" element={<AboutPage />} />
+          <Route path="download" element={<DownloadPage />} />
+          <Route path="docs" element={<DocsPage />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
