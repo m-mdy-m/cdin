dcf024df25626eee321e5eda960e443d2db2469e
kasraam <kasraam385@gmail.com>
2026-08-26 20:11:23 +0330

    fix(navbar): make it responsive and solid

---
+++ website/src/components/Navigations/DesktopView.tsx
@@ -0,0 +1,40 @@
+import { navLinks } from "@/consts/navLinks";
+import { ModeToggle } from "../mode-toggle";
+import { Button } from "../ui/button";
+
+export default function DesktopView() {
+  return (
+    <div className="md:flex hidden items-center gap-2">
+      {navLinks
+        .filter((link) => !link.external)
+        .map((link) => (
+          <Button
+            className="h-fit py-2 lg:text-lg font-normal flex items-center"
+            size={"lg"}
+            key={link.href}
+            variant="ghost"
+            asChild
+          >
+            <a href={link.href}>{link.label}</a>
+          </Button>
+        ))}
+      <ModeToggle />
+      {navLinks
+        .filter((link) => link.external)
+        .map((link) => (
+          <Button
+            className="h-fit py-2 lg:text-lg font-normal text-amber-50 flex items-center"
+            size={"lg"}
+            key={link.href}
+            variant="default"
+            asChild
+          >
+            <a href={link.href} target="_blank" rel="noopener noreferrer">
+              <img src="/github.svg" alt="github logo" className="w-5 invert" />
+              {link.label}
+            </a>
+          </Button>
+        ))}
+    </div>
+  );
+}
--- website/src/components/Navigations/Navbar.tsx
+++ website/src/components/Navigations/Navbar.tsx
@@ -1,55 +1,15 @@
 import logo from "../../../../scripts/icon.svg";
-import { Button } from "@/components/ui/button";
-import { navLinks } from "@/consts/navLinks";
-import { ModeToggle } from "@/components/mode-toggle";
+import MobileView from "./MobileView";
+import DesktopView from "./DesktopView";
 
 export default function Navbar() {
   return (
-    <nav className="pl-2 pr-8 py-6 flex items-center border-b bg-background sticky top-0 z-10 justify-between">
+    <nav className="md:pl-2 pl-0 pr-4 md:pr-8 py-2 lg:py-6 flex items-center border-b bg-background sticky top-0 z-10 justify-between">
       <a href="/" className="flex items-center ">
         <img src={logo} alt="cdin_logo" className="w-16 aspect-square" />
-        <span className="lg:text-lg -translate-x-2">cdin</span>
       </a>
-      <div className="flex items-center gap-2">
-        {navLinks
-          .filter((link) => !link.external)
-          .map((link) => (
-            <Button
-              className="h-fit py-2 lg:text-lg font-normal flex items-center"
-              size={"lg"}
-              key={link.href}
-              variant="ghost"
-              asChild
-            >
-              <a href={link.href}>{link.label}</a>
-            </Button>
-          ))}
-        <ModeToggle />
-        {navLinks
-          .filter((link) => link.external)
-          .map((link) => (
-            <Button
-              className="h-fit py-2 lg:text-lg font-normal flex items-center"
-              size={"lg"}
-              key={link.href}
-              variant="default"
-              asChild
-            >
-              <a
-                href={link.href}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="/github.svg"
-                  alt="github logo"
-                  className="w-5 invert"
-                />
-                {link.label}
-              </a>
-            </Button>
-          ))}
-      </div>
+      <MobileView />
+      <DesktopView />
     </nav>
   );
 }
