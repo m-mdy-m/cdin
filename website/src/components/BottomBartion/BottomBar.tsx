0d48251a027ae503c0f60da0f8fc42ab75e74992
kasraam <kasraam385@gmail.com>
2026-08-27 13:45:52 +0330

    fix(landing page): Home page is done & footer added fully responsive

--- website/src/components/BottomBartion/BottomBar.tsx
+++ website/src/components/BottomBartion/BottomBar.tsx
@@ -1,3 +1,38 @@
+import { footerLinks } from "@/consts/footerLinks";
+import { Button } from "../ui/button";
+
 export default function BottomBar() {
-  return <footer> footer</footer>;
+  return (
+    <footer className="">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 border-y">
+        {footerLinks.map((link) => (
+          <a
+            key={link.label}
+            className="py-10 text-center not-last:border-b sm:not-last:border-b-0 md:border-r-0 md:not-last:border-r m-0 hover:rng rounded-none hover:bg-foreground/10 duration-75"
+            href={link.href}
+            target={link.external ? "_blank" : "_self"}
+          >
+            {link.label}{" "}
+            <span className="text-muted-foreground opacity-80">
+              {link.label === "GitHub" ? "[22]" : null}
+            </span>
+          </a>
+        ))}
+      </div>
+      <div className="py-28">
+        <p className="text-center w-full">
+          MIT License Copyright (c) 2026{" "}
+          <Button
+            asChild
+            className="px-0 font-semibold text-lg"
+            variant={"link"}
+          >
+            <a target="_blank" href="https://github.com/m-mdy-m">
+              m-mdy-m
+            </a>
+          </Button>
+        </p>
+      </div>
+    </footer>
+  );
 }
---
+++ website/src/consts/footerLinks.ts
@@ -0,0 +1,21 @@
+export const footerLinks = [
+  {
+    label: "GitHub",
+    href: "https://github.com/m-mdy-m/cdin",
+    external: true,
+  },
+  {
+    label: "Docs",
+    href: "/docs",
+  },
+  {
+    label: "Changelog",
+    href: "https://github.com/m-mdy-m/cdin/blob/main/CHANGELOG.md",
+    external: true,
+  },
+  {
+    label: "Contribute",
+    href: "https://github.com/m-mdy-m/cdin?tab=contributing-ov-file",
+    external: true,
+  },
+];
