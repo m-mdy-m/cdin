878b06a20d8e05c1f4dd0f20198db5257a1a5f14
kasraam <kasraam385@gmail.com>
2026-08-30 18:46:59 +0330

    fix(docs page): redirect to github until approve from owner

--- website/src/pages/DocsPage.tsx
+++ website/src/pages/DocsPage.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Construction } from "lucide-react";
 
 export default function DocsPage() {
@@ -5,6 +6,11 @@ export default function DocsPage() {
     <div className="flex  flex-col items-center justify-center gap-2 sm:h-auto min-h-[60vh]">
       <Construction className="size-8 text-muted-foreground animate-pulse sm:size-16" />
       <p className="text-muted-foreground">Docs — under development</p>
+      <Button variant="link" asChild>
+        <a target="_blank" href="https://github.com/m-mdy-m/cdin#documentation">
+          raw docs here
+        </a>
+      </Button>
     </div>
   );
 }
