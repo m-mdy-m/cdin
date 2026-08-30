0567860496b90475e1506927b9422d081a74e71e
kasraam <kasraam385@gmail.com>
2026-08-30 19:00:28 +0330

    fix(naming): make everythin PastalCase

--- website/src/components/ModeToggle.tsx
+++ website/src/components/ModeToggle.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { useTheme } from "@/components/theme-provider";
+import { useTheme } from "@/components/ThemeProvider";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
