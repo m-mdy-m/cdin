c96c7721e8d78235187bbe68694cec3184aded76
kasraam <kasraam385@gmail.com>
2026-08-30 11:43:19 +0330

    fix(docs): fow now just placeholder

--- website/src/pages/DocsPage.tsx
+++ website/src/pages/DocsPage.tsx
@@ -1,3 +1,10 @@
+import { Construction } from "lucide-react";
+
 export default function DocsPage() {
-  return <div>docs</div>;
+  return (
+    <div className="flex  flex-col items-center justify-center gap-2 sm:h-auto min-h-[60vh]">
+      <Construction className="size-8 text-muted-foreground animate-pulse sm:size-16" />
+      <p className="text-muted-foreground">Docs — under development</p>
+    </div>
+  );
 }
