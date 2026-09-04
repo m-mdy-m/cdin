3532e9e9fc268c32b4c47a1db3278bca5bc271de
kasraam <kasraam385@gmail.com>
2026-09-04 13:31:57 +0330

    feat(DocsLayout): add and fix DocsPage Layout independment

---
+++ website/src/components/DocsComp/Header.tsx
@@ -0,0 +1,3 @@
+export default function DocsHeader() {
+  return <header className="boder-b p-10 sticky top-0">cdin</header>;
+}
--- website/src/pages/DocsPage.tsx
+++ website/src/pages/DocsPage.tsx
@@ -1,16 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Construction } from "lucide-react";
-
 export default function DocsPage() {
-  return (
-    <div className="flex  flex-col items-center justify-center gap-2 sm:h-auto min-h-[60vh]">
-      <Construction className="size-8 text-muted-foreground animate-pulse sm:size-16" />
-      <p className="text-muted-foreground">Docs — under development</p>
-      <Button variant="link" asChild>
-        <a target="_blank" href="https://github.com/m-mdy-m/cdin#documentation">
-          raw docs here
-        </a>
-      </Button>
-    </div>
-  );
+  return <div className="h-full bg-red-500"></div>;
 }
