ccb5bd7f659c339cfbb01b72ebfe9cd61c511755
kasraam <kasraam385@gmail.com>
2026-09-04 20:20:31 +0330

    feat(website): added shortcut panel to overview of docs - sticky bars

--- website/src/pages/DocView.tsx
+++ website/src/pages/DocView.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { getDocBySlug } from "@/lib/docs";
 import MarkdownRenderer from "@/components/DocsComp/MarkdownRenderer";
 import NotFound from "@/components/NotFound";
+import ShortCuts from "./ShortCuts";
 
 export default function DocView() {
   const { slug } = useParams<{ slug: string }>();
@@ -12,8 +13,13 @@ export default function DocView() {
   }
 
   return (
-    <article className="px-8 py-10 overflow-y-auto h-full">
-      <MarkdownRenderer content={doc.content} />
+    <article className="px-8 py-10 h-full gap-4 grid grid-cols-9">
+      <div className="col-span-6">
+        <MarkdownRenderer content={doc.content} />
+      </div>
+      <div className="col-span-3 relative">
+        <ShortCuts />
+      </div>
     </article>
   );
 }
