50e4a2973fb29549784bd42f558ecc7207c18347
kasraam <kasraam385@gmail.com>
2026-09-07 22:16:32 +0330

    feat(docs): added footer for docs pages to more ez navigation

---
+++ website/src/consts/docsFooterLinks.ts
@@ -0,0 +1,25 @@
+export const docsFooterLinks = [
+  {
+    label: "Back to Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about-us",
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/m-mdy-m/cdin",
+    external: true,
+  },
+  {
+    label: "Contribute",
+    href: "https://github.com/m-mdy-m/cdin?tab=contributing-ov-file",
+    external: true,
+  },
+  {
+    label: "Found a bug? Open an issue",
+    href: "https://github.com/m-mdy-m/cdin/issues",
+    external: true,
+  },
+];
